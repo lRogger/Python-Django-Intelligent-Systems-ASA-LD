1d1911a4ab901fbde5524e0ec58fce6648d1017f
mquinde <mquinde@viamatica.me>
2024-11-25 17:27:23 -0500

    feat: :sparkles: Implementacion de funcionalidad del dropdown

--- intelligent_systems/layout/urls.py
+++ intelligent_systems/layout/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
 from . import views
-from AST.views import *
-from FL.views import *
+from AST import views as ast_views
+from FL import views as fl_views
 
 urlpatterns=[
     path('', views.homeLayout, name='index'),
-    path('AST/', views.migration_AST, name='migration_AST'),
-    path('FL/', views.migration_FL_View, name='migration_FL'),
+    path('AST/', ast_views.migration_AST_View, name='migration_AST'),
+    path('FL/', fl_views.migration_FL_View, name='migration_FL'),
 ]
