60f642877214d35b06004e1dbfd0bfc54f2d96a9
mquinde <mquinde@viamatica.me>
2024-11-26 17:58:41 -0500

    feat(FormDinamic): :sparkles: Implementacion del form file, pendiente validacion de archivos

--- intelligent_systems/migration_excel/forms.py
+++ intelligent_systems/migration_excel/forms.py
@@ -1,10 +1,19 @@
 # forms.py
 
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 class CargarExcelForm(forms.Form):
-    archivo_excel = forms.FileField(label="Selecciona un archivo Excel", required=True)
+    archivo_excel_estudiantes = forms.FileField(label="Indicadores ESTUDIANTES", required=True, validators=[FileExtensionValidator(allowed_extensions=[".xlsx", "xls"])])
+    archivo_excel_profesores = forms.FileField(label="Indicadores PROFESORES", required=True, )
 
 
     class Meta:
-        fields = ('migracion')
+        fields = ('archivo_excel_estudiantes', 'archivo_excel_profesores')
+
+    def __init__(self, *args, **kwargs):
+        super(CargarExcelForm, self).__init__(*args, **kwargs)
+
+        self.fields['archivo_excel_estudiantes'].widget.attrs['class'] = 'mt-2 block w-full border-0 hover:border-blue-100 text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100'
+        self.fields['archivo_excel_profesores'].widget.attrs['class'] = 'mt-2 block w-full border-0 hover:border-blue-100 text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100'
+
